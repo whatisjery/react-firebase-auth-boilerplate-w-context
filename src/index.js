d8275ab174dd44047ce2ea6b1e9b224ce141fe27
Jérémie <jeremienallet@gmail.com>
2020-02-03 10:46:32 +0100

    router fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import "./index.css";
 
